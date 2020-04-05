919989c42a4e2403a0eb80b2926845e3ec64335f
Larry Olguin <iraqwarvet31@gmail.com>
2020-04-05 12:43:58 -0700

    First commit

---
+++ assets/js/tip.js
@@ -0,0 +1,29 @@
+// Get bill amount
+// Get Service quality
+// Get number of people sharing the bill
+
+let billAmount = document.querySelector('#bill')
+let serviceAmount = document.querySelector('#service')
+let peopleAmount = document.querySelector('#people')
+let calculateBtn = document.querySelector('#calculate');
+let tipAmount = document.querySelector('#tipAmount')
+let each = document.querySelector('#each')
+
+calculateBtn.addEventListener('click', function() {
+  let tip = (serviceAmount.value * billAmount.value) / peopleAmount.value;
+  let value = tip.toLocaleString(
+    undefined,
+    { minimumFractionDigits: 2}
+  )
+  if (peopleAmount.value > 1) {
+    each.textContent = 'each';
+  }
+  tipAmount.textContent = `$${value}`
+})
+
+
+
+
+
+
+
