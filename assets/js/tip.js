e0f466ee2f662c9b213317f1d4c4858545bb4656
Larry Olguin <iraqwarvet31@gmail.com>
2020-04-05 15:53:37 -0700

    Added tip title to Javascript file and paragraph element to html file

--- assets/js/tip.js
+++ assets/js/tip.js
@@ -7,6 +7,7 @@ let serviceAmount = document.querySelector('#service')
 let peopleAmount = document.querySelector('#people')
 let calculateBtn = document.querySelector('#calculate');
 let tipAmount = document.querySelector('#tipAmount')
+let tipTitle = document.querySelector('#tipTitle')
 let each = document.querySelector('#each')
 
 calculateBtn.addEventListener('click', function() {
@@ -18,6 +19,7 @@ calculateBtn.addEventListener('click', function() {
   if (peopleAmount.value > 1) {
     each.textContent = 'each';
   }
+  tipTitle.textContent = 'Tip Amount'
   tipAmount.textContent = `$${value}`
 })
 
